8704f45e94300939d910919fa82827a356d5a1fc
criztiandev <criztiandev@gmail.com>
2024-09-10 02:19:52 +0800

    Added Account validation

---
+++ server/src/validation/account.validation.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export const AccountValidation = z.object({
+  firstName: z
+    .string()
+    .min(1, "First name is too short")
+    .max(64, "First name is too long"),
+
+  lastName: z
+    .string()
+    .min(1, "Last name is too short")
+    .max(64, "Last name is too long"),
+
+  email: z.string().min(1, "Email is too short").max(64, "Email is too long"),
+  password: z
+    .string()
+    .min(8, "Password is too short")
+    .max(16, "Password is too long"),
+});
