c0ad6509c46627367f4804e2354c804722b595ce
criztiandev <criztiandev@gmail.com>
2024-09-10 02:53:54 +0800

    Improve registration field

--- client/src/feature/auth/page/RegisterPage.tsx
+++ client/src/feature/auth/page/RegisterPage.tsx
@@ -1,26 +1,15 @@
-import { DatePickerField } from "@/common/components/form/DatePickerField";
 import InputField from "@/common/components/form/InputField";
 import SelectField from "@/common/components/form/SelectField";
 import { Button } from "@/common/components/ui/button";
-import { Card } from "@/common/components/ui/card";
+import { Checkbox } from "@/common/components/ui/checkbox";
 import { FormBase } from "@/common/components/ui/form";
 import { XStack, YStack } from "@/common/components/ui/stack";
-import useMultiForm from "@/common/hooks/helper/useMultiform";
-import { IAccount } from "@/feature/shared/interface";
-import { EyeIcon, EyeOff } from "lucide-react";
-import { useState } from "react";
+import { IAccount, User } from "@/feature/shared/interface";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { AccountValidationList } from "@/feature/shared/validation/account.validation";
-
+import { AccountValidation } from "@/feature/shared/validation/account.validation";
+import { DatePickerField } from "@/common/components/form/DatePickerField";
 const RegisterPage = () => {
-  const { element, isFistStep, currentStep, isLastStep, nextStep, prevStep } =
-    useMultiForm([
-      <PersonalInfoStep />,
-      <OtherInfoStep />,
-      <AccountInfoStep />,
-    ]);
-
   const form = useForm<IAccount>({
     defaultValues: {
       firstName: "",
@@ -29,63 +18,91 @@ const RegisterPage = () => {
       email: "",
       password: "",
     },
-    resolver: zodResolver(AccountValidationList[currentStep]),
+    resolver: zodResolver(AccountValidation),
   });
 
-  const onSubmit = () => {
-    if (!isLastStep) {
-      nextStep();
-      return;
-    }
-
-    const result = form.getValues();
-    console.log(result);
+  const onSubmit = (value: Pick<User, "email" | "password">) => {
+    console.log(value);
   };
 
   return (
     <div className="h-screen border ">
-      <div className="h-full  flex justify-center items-center">
-        <div className="flex justify-center items-center  p-4">
-          <Card>
-            <div className="flex justify-center items-center   p-4 rounded-md">
-              <div className="  p-4 min-w-[350px] rounded-md">
-                <YStack className="mb-8">
-                  <h1 className="text-2xl font-bold text-center">
-                    Create your Free Account
-                  </h1>
-                </YStack>
-                <FormBase {...form}>
-                  <form
-                    className="flex flex-col w-full"
-                    onSubmit={form.handleSubmit(onSubmit)}
-                  >
-                    {element}
+      <div className="h-full grid grid-cols-2 gap-4">
+        <div className=""></div>
+        <div className="flex justify-center items-center  p-4 border w-full">
+          <div className="max-w-[500px] w-full">
+            <div className="  p-4 min-w-[350px] w-full  rounded-md">
+              <YStack className="gap-2 mb-8">
+                <h1 className="text-3xl font-bold">Create an Account</h1>
+                <span>We would like to know you</span>
+              </YStack>
+              <FormBase {...form}>
+                <form
+                  className="flex flex-col gap-4 w-full"
+                  onSubmit={form.handleSubmit(onSubmit)}
+                >
+                  <InputField
+                    type="text"
+                    label="First Name"
+                    name="firstName"
+                    placeholder="Enter your First Name"
+                  />
+                  <InputField
+                    type="text"
+                    label="Last Name"
+                    name="lastName"
+                    placeholder="Enter your Last Name"
+                  />
 
-                    <YStack className="gap-4 my-4">
-                      <Button type={"submit"}>
-                        {isLastStep ? "Submit" : "Next"}
-                      </Button>
-                      {!isFistStep && (
-                        <Button
-                          variant="outline"
-                          type="button"
-                          onClick={() => prevStep()}
-                        >
-                          Back
-                        </Button>
-                      )}
-                    </YStack>
-                    <XStack className="gap-2 items-center justify-center">
-                      <span>Already have account ?</span>
-                      <a href="/" className="text-blue-600">
-                        Login
-                      </a>
+                  <div className="grid grid-cols-2 gap-4">
+                    <DatePickerField name="bod" label="Date of Birth" />
+                    <SelectField
+                      label="Gender"
+                      name="gender"
+                      placeholder="Select Gender"
+                      options={[
+                        { label: "Male", value: "male" },
+                        { label: "Female", value: "female" },
+                      ]}
+                    />
+                  </div>
+
+                  <InputField
+                    type="email"
+                    label="Email"
+                    name="email"
+                    placeholder="Enter your Email"
+                  />
+                  <InputField
+                    type="password"
+                    label="Password"
+                    name="password"
+                    placeholder="Enter your Password"
+                  />
+
+                  <XStack className="items-center my-1">
+                    <XStack className="gap-2 items-center ">
+                      <Checkbox />
+                      <XStack className="space-x-2">
+                        <span>I accept</span>
+                        <a href="#" className="text-blue-600">
+                          Terms and Condition
+                        </a>
+                      </XStack>
                     </XStack>
-                  </form>
-                </FormBase>
-              </div>
+                  </XStack>
+
+                  <Button className="mb-8">Login</Button>
+                  <XStack className="gap-2 items-center justify-center">
+                    <span>Don't have an account</span>
+                    <a href="/register" className="text-blue-600">
+                      Create an account
+                    </a>
+                  </XStack>
+                </form>
+              </FormBase>
             </div>
-          </Card>
+          </div>
         </div>
       </div>
     </div>
@@ -93,72 +110,3 @@ const RegisterPage = () => {
 };
 
 export default RegisterPage;
-
-const PersonalInfoStep = () => {
-  return (
-    <div className="flex flex-col gap-2">
-      <InputField
-        type="text"
-        label="First Name"
-        name="firstName"
-        placeholder="Enter your First Name"
-      />
-      <InputField
-        type="text"
-        label="Last Name"
-        name="lastName"
-        placeholder="Enter your Last Name"
-      />
-    </div>
-  );
-};
-
-const OtherInfoStep = () => {
-  return (
-    <div className="flex flex-col gap-2">
-      <DatePickerField
-        name="bod"
-        label="Date of Birth"
-        description="Your date of birth is used to calculate your age"
-      />
-      <SelectField
-        label="Gender"
-        name="gender"
-        placeholder="Select Gender"
-        options={[
-          { label: "Male", value: "male" },
-          { label: "Female", value: "female" },
-        ]}
-      />
-    </div>
-  );
-};
-
-const AccountInfoStep = () => {
-  const [isShowPassword, setIsShowPassword] = useState(false);
-  return (
-    <div className="flex flex-col gap-2">
-      <InputField
-        type="email"
-        label="Email"
-        name="email"
-        placeholder="Enter your Email"
-      />
-      <InputField
-        type={isShowPassword ? "text" : "password"}
-        label="Password"
-        name="password"
-        placeholder="Enter your Password"
-        icon={
-          isShowPassword ? (
-            <EyeOff size={22} opacity={0.5} />
-          ) : (
-            <EyeIcon size={22} opacity={0.5} />
-          )
-        }
-        dir="right"
-        onPress={() => setIsShowPassword((prev) => !prev)}
-      />
-    </div>
-  );
-};
