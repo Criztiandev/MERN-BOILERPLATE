69097864e008d28d27244c0f981abec5dc1e25a9
criztiandev <criztiandev@gmail.com>
2024-09-17 03:13:50 +0800

    Added Sonner toast

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "./common/components/ui/sonner.tsx";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <Toaster richColors toastOptions={{}} />
     </QueryClientProvider>
   </StrictMode>
 );
