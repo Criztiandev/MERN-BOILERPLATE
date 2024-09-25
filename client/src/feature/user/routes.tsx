991942cfee32034046e17dfa59838a595237b0e9
criztiandev <criztiandev@gmail.com>
2024-09-25 08:41:20 +0800

    Added routes default file for both roles

---
+++ client/src/feature/user/routes.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import Homepage from "./page/Homepage";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+]);
+
+export default router;
